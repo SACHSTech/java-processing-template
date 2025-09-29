945387ae3e779277f5aa4b43510557c79f222ab7
Dave Cheng <159358380+davecheng-tech@users.noreply.github.com>
2025-09-29 10:51:19 -0400

    Update Sketch.java with PNG file output

--- Sketch.java
+++ Sketch.java
@@ -2,31 +2,46 @@ import processing.core.PApplet;
 
 /**
  * Starter for graphics assignments.
- * Edit your code in the setup() and draw() functions below.
+ * Edit the setup() and draw() functions.
+ *
+ * Default: Saves one frame to image.png (File Mode).
  */
 public class Sketch extends PApplet {
 
-  /**
-   * Called once when program is run. Put your size() method here.
-   */  
+    // Change flag for output to screen instead of file
+    private static boolean screenMode = false;
+  
+    // Called once when program is run. Put your canvas size() here.
     public void settings() {
         size(600, 600);
     }
 
-  /** 
-   * Called once when program is run. Add initial setup
-   * values here, e.g., background, stroke, fill etc.
-   */
+    // Called once when run. Add initial setup values here, e.g., background()
     public void setup() {
         background(210, 255, 173);
     }
 
-  /**
-   * Called repeatedly. Anything drawn to the screen should go here.
-   */
+    // Called repeatedly when run. Draw your stuff here
     public void draw() {
-        // === SAMPLE CODE, REPLACE THIS STUFF ===
+        // === BEGIN CODE HERE, REPLACE THIS STUFF ===
         stroke(237, 47, 228);
         line(150, 25, 270, 350);
+        
+        // === END CODE HERE, DO NOT EDIT BELOW THIS ===
+        if (!screenMode) {
+            save("image.png");
+            exit();
+        }
+
+    /**
+     * Launches the sketch.
+     * Default: File Mode (saves image.png).
+     * Use --screen to run in Screen Mode.
+     */
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("--screen")) {
+            screenMode = true;
+        }
+        PApplet.runSketch(new String[]{"Sketch"}, new Sketch());
     }
 }
